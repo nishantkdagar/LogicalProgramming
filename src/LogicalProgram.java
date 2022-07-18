96db208de3b75ce1063634b60f07baddfeba602e
Nishan Kumar <nishantkumarkd@gmail.com>
2022-07-18 09:04:40 +0530

    Fibonacci Series

--- src/LogicalProgram.java
+++ src/LogicalProgram.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class LogicalProgram {
-    public static void main(String[] args){
-        System.out.println("Trying Some Logical Programming");
+    public static void Fibonacci(){
+        int n1=0,n2=1,i,n3,n;
+        Scanner r=new Scanner(System.in);
+        System.out.print("Enter Limit: ");
+        n=r.nextInt();
+        System.out.print("Fibonacci Series: "+n1+" "+n2);
+        for (i=2;i<=n;i++)
+        {
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.print(" "+n3);
+        }
+    }
+    public static void main(String[] args) {
+        Fibonacci();
     }
 }
